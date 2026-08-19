437a0dc52c073ad9afda9d56afc086556c0c7d78
AhmedAdel <aa3452000@gmail.com>
2026-08-19 19:43:18 +0300

    Result

--- EmployeeManagementSystemGenerics/Common/Result.cs
+++ EmployeeManagementSystemGenerics/Common/Result.cs
@@ -4,7 +4,27 @@ using System.Text;
 
 namespace EmployeeManagementSystemGenerics.Common
 {
-    internal class Result
+    public class Result<T>
     {
+        public bool Success { get; private set; }
+        public string Message { get; private set; }
+        public T? Data { get; private set; }
+
+        private Result (bool success, string message, T? data   )
+        {
+            Success = success;
+            Message = message;
+            Data = data;
+        }
+
+        public static Result<T> Ok (T data, string message)
+        {
+            return new Result<T>(true, message, data);
+        }
+
+        public static Result<T> Fail(string message)
+        {
+            return new Result<T>(false, message, default);
+        }
     }
 }
