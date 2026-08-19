036bf69f60473d4645431cdfaa2fd2a8fae2be12
AhmedAdel <aa3452000@gmail.com>
2026-08-19 19:00:15 +0300

    Manager

--- EmployeeManagementSystemGenerics/Models/Employee.cs
+++ EmployeeManagementSystemGenerics/Models/Employee.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace EmployeeManagementSystemGenerics.Models
 {
-    internal class Employee
+    public class Employee
     {
         public int Id { get; set; }
         public string Name { get; set; }    
--- EmployeeManagementSystemGenerics/Models/Manager.cs
+++ EmployeeManagementSystemGenerics/Models/Manager.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace EmployeeManagementSystemGenerics.Models
 {
-    internal class Manager
+    public class Manager : Employee
     {
-    }
+        public List<Employee> TeamMembers { get; set; }
+
+        public Manager (int id,
+                        string name,
+                        DateTime hireDate,
+                        int departmentId,
+                        decimal salary) : base(id, name, hireDate, departmentId, salary)
+        {
+            TeamMembers = new List<Employee>();
+        }
+    }                   
 }
