864fb3a653ca857453c5dbd1d69232da68f7774a
AhmedAdel <aa3452000@gmail.com>
2026-08-19 19:49:47 +0300

    EmployeeEventArgs

--- EmployeeManagementSystemGenerics/Events/EmployeeEventArgs.cs
+++ EmployeeManagementSystemGenerics/Events/EmployeeEventArgs.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using EmployeeManagementSystemGenerics.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace EmployeeManagementSystemGenerics.Events
 {
-    internal class EmployeeEventArgs
+    public class EmployeeEventArgs : EventArgs
     {
+        public Employee Employee {  get; set; }
+
+        public EmployeeEventArgs (Employee employee)
+        {
+            Employee = employee;
+        }
     }
 }
